ab0f3f8dbee4a5f8c19bdd9ad80efb8b24234279
alfianaf <alfianaf123@gmail.com>
2019-12-04 20:59:18 +0800

    validator new

---
+++ app/src/main/java/com/example/projectalpha/Activity/ValidatorActivity/DetailValidatorActivity.java
@@ -0,0 +1,16 @@
+package com.example.projectalpha.Activity.ValidatorActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projectalpha.R;
+
+public class DetailValidatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_validator);
+    }
+}
---
+++ app/src/main/java/com/example/projectalpha/Activity/ValidatorActivity/MainValidatorActivity.java
@@ -0,0 +1,16 @@
+package com.example.projectalpha.Activity.ValidatorActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projectalpha.R;
+
+public class MainValidatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_validator);
+    }
+}

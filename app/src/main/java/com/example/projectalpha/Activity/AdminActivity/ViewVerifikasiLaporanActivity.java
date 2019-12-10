07373b82bbf85f2aa0fc50a56d3219e4b9e9c5e6
Favo Perdana H.S <perdanafavo97@gmail.com>
2019-12-11 05:11:52 +0700

    Layout view validator + rombak semua recyclerview

---
+++ app/src/main/java/com/example/projectalpha/Activity/AdminActivity/ViewVerifikasiLaporanActivity.java
@@ -0,0 +1,16 @@
+package com.example.projectalpha.Activity.AdminActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projectalpha.R;
+
+public class ViewVerifikasiLaporanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_verifikasi_laporan);
+    }
+}

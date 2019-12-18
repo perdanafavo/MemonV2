c53b50bf229b144ed349a5e880411e0b9db4ebb1
Favo Perdana H.S <perdanafavo97@gmail.com>
2019-12-18 07:51:29 +0700

    Layout batere kering dan basah

---
+++ app/src/main/java/com/example/projectalpha/Activity/UsersActivity/BatereSelectActivity.java
@@ -0,0 +1,15 @@
+package com.example.projectalpha.Activity.UsersActivity;
+
+import android.os.Bundle;
+
+import com.example.projectalpha.Helpers.CustomCompatActivity;
+import com.example.projectalpha.R;
+
+public class BatereSelectActivity extends CustomCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batere_select);
+    }
+}
